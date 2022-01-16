0db40a075fa96e6a7ec124c5f75126a2d1abfca2
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-16 16:57:07 +0900

    audioplayers追加・インポート

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -10,7 +10,11 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Text(nouns.first),
+            child: FlatButton(
+              onPressed: () {
+              },
+              child: Text('Click Me!'),
+            ),
           ),
         ),
       ),
